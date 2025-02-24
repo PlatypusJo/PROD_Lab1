6d6728e391dc3b6d8136f8da77dbe510671c7ec4
PlatypusJo <maxim_muravyov@mail.ru>
2025-02-24 23:10:08 +0300

    Add fields in settings model; Make HeatSolver

--- Model/HeatSettingsModel.cs
+++ Model/HeatSettingsModel.cs
@@ -9,5 +9,53 @@ namespace Lab1.Model
     public class HeatSettingsModel
     {
 
+        #region Поля
+
+        private double _aBoundary = 0;
+        private double _aaBoundary = 0;
+
+        private double _bBoundary = 0;
+        private double _bbBoundary = 0;
+
+        private double _cBoundary = 0;
+        private double _ccBoundary = 0;
+
+        // Размеры параллелепипеда в см
+        private double _iParallelepipedSize = 100;
+        private double _jParallelepipedSize = 100;
+        private double _kParallelepipedSize = 100;
+
+        // Шаги и коэффициент
+        private double h = 0.01;
+        private double tau = 0.01;
+        private double a = 0.03;
+
+        private double _initTime = 0;
+        private double _maxTime;
+
+        // Условие устойчивости
+        private bool _isStable => (tau * a * a) / (h * h) < 0.125;
+
+        #endregion
+
+        #region Свойства
+
+        public int IDimSize => (int)(_iParallelepipedSize / h);
+        public int JDimSize => (int)(_jParallelepipedSize / h);
+        public int KDimSize => (int)(_kParallelepipedSize / h);
+
+        #endregion
+
+        #region Конструктор
+
+        public HeatSettingsModel()
+        {
+            
+        }
+
+        #endregion
+        
+
+       
     }
 }
--- Services/HeatSolver.cs
+++ Services/HeatSolver.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Lab1.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,85 @@ namespace Lab1.Services
 {
     public class HeatSolver
     {
+        #region Поля
+
+        private double[,,] _u;
+
+        private double[,,] _uNew;
+
+        private int _iDim;
+
+        private int _jDim;
+
+        private int _kDim;
+
+        private HeatSettingsModel _settings;
+
+        #endregion
+
+        #region Конструкторы
+
+        public HeatSolver(HeatSettingsModel settings)
+        {
+            _settings = settings;
+
+            _iDim = _settings.IDimSize;
+            _jDim = _settings.JDimSize;
+            _kDim = _settings.KDimSize;
+
+            InitializeTemperature();
+        }
+
+        #endregion
+
+        #region Методы
+
+        public double[,,,] Step()
+        {
+            return null;
+        }
+
+        public double[,,,] StepParallel()
+        {
+            return null;
+        }
+
+        public double CalculateTemperature()
+        {
+            return 0;
+        }
+
+        public void UpdateHeatSettings(HeatSettingsModel settings)
+        {
+            if (settings is null)
+                return;
+
+            _settings = settings;
+
+            _iDim = _settings.IDimSize;
+            _jDim = _settings.JDimSize;
+            _kDim = _settings.KDimSize;
+
+            InitializeTemperature();
+        }
+
+        #endregion
+
+        #region Внутренние методы
+
+        private void InitializeTemperature()
+        {
+            _u = new double[_iDim, _jDim, _kDim];
+
+            for (int i = 0; i < _iDim; i++)
+                for (int j = 0; j < _jDim; j++)
+                    for (int k = 0; k < _kDim; k++)
+                        _u[i, j, k] = 0;
+
+            _uNew = _u;
+        }
+
+        #endregion
+
     }
 }
